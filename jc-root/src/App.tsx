2bd42f3a05ab5c8807f79b493b25639a565ef9ef
janeya0 <jane.yao321@gmail.com>
2022-12-21 22:03:49 -0500

    Create react app foundation,
    Add tailwind, sass packages
    
    Add Home page and styling

--- jc-root/src/App.tsx
+++ jc-root/src/App.tsx
@@ -1,14 +1,13 @@
 import Text from './components/Text'
 import Button from './components/Button'
 import Centrepiece from './components/Centrepiece'
+import Home from './pages/Home'
+import './index.scss';
 
 function App() {
-  const centerPic = require('./images/homepic.png');
   return (
     <div >
-      <Text text='contact' color='grey' />
-      <Centrepiece photo={centerPic}></Centrepiece>
-
+      <Home />
     </div>
   );
 }
